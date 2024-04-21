d1f64c350830f63c7c2d7a5b5065dc4a1cc487b1
Art <absolutus11@gmail.com>
2024-04-21 12:31:23 +0400

    feat: add car service

---
+++ src/services/CarService.ts
@@ -0,0 +1,11 @@
+const getCars = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return [];
+  }
+};
+
+export default getCars;
