8f96eed1fe3f46b13e2aafd1deff2fa8cf3d7f7b
Art <absolutus11@gmail.com>
2024-04-27 02:18:30 +0400

    refactor: remove sgvr package

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import svgr from 'vite-plugin-svgr';
-
-svgr({
-  include: '**/*.svg',
-});
 
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
 });
