85c76a9ca1c54c967b59f5e45a31a234c282dcfd
Art <absolutus11@gmail.com>
2024-04-27 02:21:23 +0400

    fix: add proper setstate types

--- types.d.ts
+++ types.d.ts
@@ -6,18 +6,20 @@ type CarData = {
 
 type FetchCars = (setCars: SetCars, setError: SetError) => void;
 
-type SetCars = (cars: CarData[]) => void;
+type SetState = Dispatch<S | SetStateAction<S>>;
 
-type SetError = (error: string | null) => void;
+type SetCars = Dispatch<CarData[] | SetStateAction<CarData[]>>;
+
+type SetError = Dispatch<string | null | SetStateAction<string | null>>;
 
 type SetSelected = (id: number) => void;
 
 type NewCarData = { name: string; color: string };
 
-type AddCar = (payload: NewCarData, cars: CarData[], setCars: SetCars, setError: SetError) => void;
+type AddCar = (payload: NewCarData, setCars: SetCars, setError: SetError) => void;
 
-type UpdateCar = (car: CarData, cars: CarData[], setCars: SetCars, setError: SetError) => void;
+type UpdateCar = (car: CarData, setCars: SetCars, setError: SetError) => void;
 
-type RemoveCar = (id: number, cars: CarData[], setCars: SetCars, setError: SetError) => void;
+type RemoveCar = (id: number, setCars: SetCars, setError: SetError) => void;
 
 type InputSetter = (value: string) => void;
